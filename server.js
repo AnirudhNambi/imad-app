df40396ee54299f66009dcb4da3390146dc16184
AnirudhNambi <30660765+AnirudhNambi@users.noreply.github.com>
2017-08-09 16:28:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/result', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'result.html'));
+});
 app.get('/details',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
